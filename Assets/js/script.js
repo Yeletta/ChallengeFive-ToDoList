012330d71f995b8f2d94dd1496cc995e2378209a
YeBa <yeletta.banks@gmail.com>
2022-08-08 19:53:00 -0700

    Initial commit

---
+++ Assets/js/script.js
@@ -0,0 +1,16 @@
+var today = moment().format ("MMM Do, YYYY, h:mm:ss a");
+$("#currentDay").text(today);
+var hour = moment().format("h" +"a" );
+console.log (moment().hours());
+
+var a= moment().startOf('day').fromNow();
+console.log(a);
+//function timecheck() { 
+ //   if (hour = moment(h))
+   // then }
+    
+   // else if (hour < moment(h)){}  
+  // 
+   // else()
+//
+
